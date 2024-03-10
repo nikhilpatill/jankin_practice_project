31f4f0db24d45e3022bb80acbc90e2274251611d
nikhil <nikhilpatil1050gmail.com>
2024-03-10 12:07:25 +0530

    salman change

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		
-
+      System.out.println("this is salman changes ");
 		System.out.println("this is katrina code don't check it");
 		System.out.println("this chsnge by katrina 9-03-2024");
 		System.out.println("this is the katrina ");
